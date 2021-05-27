beeb1e5821e294fbf4dde21dddc9a78fcd05e65e
David Ferreira <dferreira@MacBook-Pro.fios-router.home>
2021-05-27 19:12:55 -0400

    API keys and stock api fetch

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,19 @@
-// test code 
+//API Key for TwelveData: 86e36b55bf3249baa6a35b1ae39ede69
+
+//API Key for Polygon.io: 6GrCrAyGOSpscsyzmo4hVcfw6OJse4D1
+
+
+// API fetch request for getting STOCK information
+function getStock (){
+    fetch(
+        'https://api.twelvedata.com/time_series?symbol=AAPL&interval=1min&type=stock&format=JSON&apikey=86e36b55bf3249baa6a35b1ae39ede69'
+    )
+    .then(function (response){
+        return response.json();
+    })
+    .then(function (data){
+        console.log(data);
+    })
+}
+
+getStock();
