a5cbd4a1ee215825038fd48d4014572b2c9a2587
David Ferreira <dferreira@MacBook-Pro.fios-router.home>
2021-05-29 10:17:15 -0400

    fetch news API

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,21 @@
-// test code 
+//API Key for TwelveData: 86e36b55bf3249baa6a35b1ae39ede69
+
+//API Key for Polygon.io: 6GrCrAyGOSpscsyzmo4hVcfw6OJse4D1
+
 var stockInputEl = document.querySelector("#stock");
 var modalEl1 = document.querySelector("#modal1");
 var modalEl2 = document.querySelector("#modal2");
 var userFormEl = document.querySelector("#user-form");
+var stockname;
 modalEl1.style.display = "none";
 modalEl2.style.display = "none";
 
 var searchStock = function (event) {
     event.preventDefault();
-
+    
     // get value from input search
-    var stockname = stockInputEl.value.trim();
-
+    stockname = stockInputEl.value.trim();
+    
     if (stockname) {
         getStockInfo(stockname);
         stockInputEl.value = "";
@@ -26,7 +30,6 @@ var getStockInfo = function (userStock) {
     // stcok api function fetch
     var apiKey = "28c7003db6dc4105bd96e1de1fe13c21";
     var apiUrl = "https://api.twelvedata.com/time_series?symbol=" + userStock + ",MSFT,EUR/USD,SBUX,NKE&interval=1min&apikey=" + apiKey;
-
     fetch(apiUrl).then(function (response) {
         if (response.ok) {
             response.json().then(function(data) {
@@ -35,31 +38,30 @@ var getStockInfo = function (userStock) {
         } else {
             modal2.style.display = "block";
         }
+        getNews();
     })
+    console.log(stockname);
 }
 
 // Click event listener
 userFormEl.addEventListener("submit", searchStock);
 
 
+// variable that will grab today's date to display recent news articles
+var todayDate = new Date().toJSON().slice(0,10);
+console.log(todayDate);
 
-
-//API Key for TwelveData: 86e36b55bf3249baa6a35b1ae39ede69
-
-//API Key for Polygon.io: 6GrCrAyGOSpscsyzmo4hVcfw6OJse4D1
-
-
-// API fetch request for getting STOCK information
-//function getStock (){
-//    fetch(
-//        'https://api.twelvedata.com/time_series?symbol=AAPL&interval=1min&type=stock&format=JSON&apikey=86e36b55bf3249baa6a35b1ae39ede69'
-//    )
-//    .then(function (response){
-//        return response.json();
-//    })
-//   .then(function (data){
-//        console.log(data);
-//    })
-// }
-
-// getStock();
+// API FETCH REQUEST FOR NEWS ARTICLES RELATED TO USER STOCK
+function getNews (){
+    var apiPoly = "6GrCrAyGOSpscsyzmo4hVcfw6OJse4D1";
+    fetch(
+        'https://api.polygon.io/v2/reference/news?limit=10&order=descending&sort=published_utc&ticker=' + stockname + '&published_utc.gte=' + todayDate + '&apiKey=' + apiPoly
+        )
+        .then(function (response){
+            return response.json();
+        })
+        .then(function (data){
+            console.log(data);
+        })
+        console.log(stockname);
+    }
